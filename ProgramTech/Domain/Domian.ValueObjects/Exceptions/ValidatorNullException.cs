7dc900a6d571da6fc13c871ca64476cd52545fd2
vikoritsa <v.savchenko95@sibstrin.ru>
2026-05-30 06:51:31 +0700

    Созданы исключения и валидаторы

---
+++ ProgramTech/Domain/Domian.ValueObjects/Exceptions/ValidatorNullException.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.ValueObjects.Exceptions;
+
+public class ValidatorNullException(string paramName)
+: ArgumentNullException(paramName, $"Validator \"{paramName}\" must be specified for type.");
